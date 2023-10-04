253e57ee8ae4b2381d24d5bfe6dd9dbdd29624a2
amirbourasEsprit <bouras.amir@esprit.tn>
2023-10-04 10:11:18 +0100

    ajout commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -24,7 +24,7 @@ public class User implements Serializable {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
-	
+	//Test 
 	private String firstName; 
 	
 	private String lastName;
